a6cd12c285b544bce79a0b90d9da0dd5ad113ff6
asalmer <asalmeri@2orgu.com>
2025-03-24 21:16:44 +0100

    fix profile

--- src/components/users/profile-card.tsx
+++ src/components/users/profile-card.tsx
@@ -57,16 +57,16 @@ export const ProfileCard = ({profile} : {profile : Session}) => {
                     </div>
                     <div className="flex-1">   
                         <div className="flex flex-col">
-                    <label>Bio</label>
+                    {/* <label>Bio</label>
                     <textarea
                     disabled
                     value={profile.user.bio}
                     //className="bg-transparent text-lg mr-6 disabled:text-white"
                     placeholder='nothing here'
                     className="p-2 h-36 bg-transparent invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 disabled:border-gray-200 bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20"
-                    >
+                    > */}
 
-                        </textarea>
+                        {/* </textarea> */}
                 </div>
             </div>
                             
